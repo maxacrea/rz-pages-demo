d6bf943bc1b1c6a033727f2a549f0c21acb27a19
maxacrea <rah.zandry_dev@yahoo.fr>
2024-10-06 23:52:04 +0300

    edit app .js

--- src/App.js
+++ src/App.js
@@ -5,21 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Toutle.mg</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Une belle chose ici pour bientôt !
         </p>
-        <p>
-          Test Deploy with Andry !!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );

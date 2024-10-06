915b86bd14fc313cdb7c4187f9eb0537204aa291
maxacrea <rah.zandry_dev@yahoo.fr>
2024-10-06 23:59:03 +0300

    add facebook logo end link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import logo from './logo.svg';
+import logo from './logo-facebook.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Toutle.mg</h1>
-        <p>
-          Une belle chose ici pour bientôt !
-        </p>
-      </header>
-    </div>
-  );
+		<div className="App">
+			<header className="App-header">
+				<h1>
+					Toutle.mg sur{" "}
+					<a href="https://web.facebook.com/profile.php?id=61552605450557">
+						<img src={logo} alt="" />
+					</a>{" "}
+				</h1>
+				<p>Une belle chose ici pour bientôt !</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;

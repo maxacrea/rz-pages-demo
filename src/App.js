2681eb757477b0186c7730c193b770467b6d4213
maxacrea <rah.zandry_dev@yahoo.fr>
2024-10-07 00:13:49 +0300

    edit style

--- src/App.js
+++ src/App.js
@@ -5,13 +5,31 @@ function App() {
   return (
 		<div className="App">
 			<header className="App-header">
-				<h2>🚀 Toutela arrive bientôt ! 🚀 </h2>
-        <p>
-        Passionnées de mode et de luxe, préparez-vous à découvrir <b>Toutela</b>, votre nouvelle boutique en ligne dédiée à la <b>mode féminine de seconde main européenne</b>. Retrouvez des vêtements et chaussures de luxe d’occasion, soigneusement sélectionnés et aussi beaux que neufs ! 👠👗✨</p>
-        <p><b>Notre site est en construction</b> et sera bientôt prêt à vous offrir une expérience shopping exceptionnelle. En attendant, restez connectées avec nous sur Facebook pour ne rien manquer des nouveautés, offres exclusives, et du lancement officiel !</p>
-        <a href="https://web.facebook.com/profile.php?id=61552605450557">👉 Rejoignez-nous sur Facebook <img src={logo} alt="" /></a> 
-        <p><b>#ModeDeLuxe
-				#Toutela #SecondHandLuxury #ShoppingOnline #FashionLovers</b></p>
+				<div>
+					<h2>🚀 Toutela arrive bientôt ! 🚀 </h2>
+					<p>
+						Passionnées de mode et de luxe, préparez-vous à découvrir{" "}
+						<b>Toutela</b>, votre nouvelle boutique en ligne dédiée à la{" "}
+						<b>mode féminine de seconde main européenne</b>. Retrouvez des
+						vêtements et chaussures de luxe d’occasion, soigneusement
+						sélectionnés et aussi beaux que neufs ! 👠👗✨
+					</p>
+					<p>
+						<b>Notre site est en construction</b> et sera bientôt prêt à vous
+						offrir une expérience shopping exceptionnelle. En attendant, restez
+						connectées avec nous sur Facebook pour ne rien manquer des
+						nouveautés, offres exclusives, et du lancement officiel !
+					</p>
+					<a className='fb' href="https://web.facebook.com/profile.php?id=61552605450557">
+						👉 Rejoignez-nous sur Facebook <img src={logo} alt="" />
+					</a>
+					<p>
+						<b>
+							#ModeDeLuxe #Toutela #SecondHandLuxury #ShoppingOnline
+							#FashionLovers
+						</b>
+					</p>
+				</div>
 			</header>
 		</div>
 	);
